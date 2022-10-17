8730b1c43cd887a2b865bb8a25d7a9e60530634b
David Henzl <david.henzl@plus4u.net>
2022-10-17 18:33:05 +0200

    after 126 section

---
+++ src/js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdPPgUYJWLQqVFMhnqQcXlk3Ik_sURPRM',
+  authDomain: 'noteballs-200c4.firebaseapp.com',
+  projectId: 'noteballs-200c4',
+  storageBucket: 'noteballs-200c4.appspot.com',
+  messagingSenderId: '1078822166247',
+  appId: '1:1078822166247:web:763969273d9f86827c8c42'
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+	db
+}
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,21 +1,34 @@
 import { defineStore } from 'pinia'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/js/firebase'
 
 export const useStoreNotes = defineStore('storeNotes', {
 	state: () => {
 		return {
 			notes: [
-				{
-					id: 'id1',
-					content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam eveniet optio molestias a doloremque sapiente corrupti, adipisci, fugiat vitae perferendis odit aliquam quos veniam praesentium culpa deleniti iste obcaecati dolores!'
-				},
-				{
-					id: 'id2',
-					content: 'This is a shorter note'
-				}
+				// {
+				// 	id: 'id1',
+				// 	content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam eveniet optio molestias a doloremque sapiente corrupti, adipisci, fugiat vitae perferendis odit aliquam quos veniam praesentium culpa deleniti iste obcaecati dolores!'
+				// },
+				// {
+				// 	id: 'id2',
+				// 	content: 'This is a shorter note'
+				// }
 			]
 		}
 	},
 	actions: {
+		async getNotes() {
+			const querySnapshot = await getDocs(collection(db, 'notes'))
+			querySnapshot.forEach((doc) => {
+				let note = {
+					id: doc.id,
+					content: doc.data().content
+				}
+				console.log(note)
+				this.notes.push(note)
+			});
+		},
 		addNote(newNoteContent) {
 			let currentDate = new Date().getTime()
 			let id = currentDate.toString()
@@ -31,7 +44,6 @@ export const useStoreNotes = defineStore('storeNotes', {
 		updateNote(id, content) {
 			let index = this.notes.findIndex(note => note.id === id)
 			this.notes[index].content = content
-
 		}
 	},
 	getters: {
